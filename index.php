f02cbd7ee46314f3fb21205b1a378b0fab49c24b
Tamara <tamararikic11@gmail.com>
2022-07-11 00:08:41 +0200

    Izmenjena pocetna stranica

--- index.php
+++ index.php
@@ -1,73 +1,35 @@
 <?php
 
-require "dbBroker.php";
-require "model/user.php";
-
-session_start();
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    $uname = $_POST['username'];
-    $upass = $_POST['password'];
 
 
-    $korisnik = new User(1, $uname, $upass);
+include('glavna.php');
 
-    $odg = User::logInUser($korisnik, $conn);
 
-    if ($odg->num_rows == 1) {
-        echo  `
-        <script>
-        console.log( "Uspešno ste pristupili sistemu");
-        </script>
-        `;
-        $_SESSION['user_id'] = $korisnik->id;
-        header('Location: home.php');
-        exit();
-    } else {
-        echo `
-        <script>
-        console.log( "Neuspešna prijava na sistem");
-        </script>
-        `;
-    }
-}
 
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+<div class = 'quote'>
+       
 
-    <title>Pocetna stranica-LogIn</title>
-</head>
+       
 
-<body>
+              <blockquote>
+            <h1><b>Dobro došli na sajt Opšte bolnice Zvornik!</b></h1>
+                 <br></br>
+                <p>Na ovom sajtu možete se informisati o lekovima koji postoje na nabavnoj listi Opšte bolnice Zvornik. 
+                      
+                  <p> Izborom odeljenja za koje naručujete lekove započinjete proces nabavke. <br> 
+                     Zatim u okviru kartice Lekovi, birate željene lekove i njihove količine i klikom na Završi obavili ste datu narudžbinu. </br>
+                      Lekovi koje ste poručili će se prikazati u tabeli. U tabeli možete brisati, dodavati i izmeniti artikle. 
+                   </p>
+                </p>
+              </blockquote>
 
-    <div class="center">
-        <h1>Opšta bolnica Zvornik</h1>
-        
-        <form method="POST" action="#">
-            <div class="inputbox">
-                <input type="text" required  class="input" id="input_text" name="username">
-                <span>Korisničko ime </span>
-            </div>
-            <div class="inputbox">
-                <input type="text" required  class="input" id="input_text"  name="password">
-                <span>Šifra </span>
-            </div>
-            <div class="inputbox">
-                <input type="submit" class="btn btn-primary" name="submit" value="Prijavi se">
+              <p><i>Informacije na broj: O64/998-38-55 
+                    <br> Email: tamararikic11@gmail.com </br>
+              </i></p>
 
-            </div>
-        </form>
-    
-    </div>
-
-</body>
-
-</html>
+       
+ </div>
